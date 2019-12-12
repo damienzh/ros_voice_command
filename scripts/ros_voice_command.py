3e2ccd8a3aad09c0131aaf806886d0cf9bbc20c8
damienzh <damienzh@gmail.com>
2019-12-12 17:17:45 +0800

    commit

--- scripts/ros_voice_command.py
+++ scripts/ros_voice_command.py
@@ -1,4 +1,10 @@
 import rospy
 import speech_recognition as sr
 
-class 
+
+class ROSSpeechRecognizer:
+
+    def __init__(self, mic_idx=0):
+        self.Recognizer = sr.Recognizer()
+        self.Mic = sr.Microphone(device_index=mic_idx)
+        self.language = 'en-US'
